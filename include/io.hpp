ba88c7cb23cd43f391d7d9feac8f11f5cf99830a
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2020-05-29 22:32:37 +0600

    Read input file containing polygons and bean width

--- include/io.hpp
+++ include/io.hpp
@@ -1,5 +1,17 @@
+#ifndef IO_HPP
+#define IO_HPP
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "geometry.hpp"
+
+using namespace std;
 
 namespace io
 {
-void testFunction();
+vector<polygon> readInputFile(string filename, double &width);
 };
+
+#endif // IO_HPP
--- src/io.cpp
+++ src/io.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 
 #include "io.hpp"
-#include "geometry.hpp"
 
-void io::testFunction()
+vector<polygon> io::readInputFile(string filename, double &width)
 {
-    std::cout << "Test ok\n";
+    cout << "Opening file: " << filename << " ....." << endl;
+    ifstream file(filename);
+    if (file.fail())
+    {
+        cerr << "File does not exists" << endl;
+        exit(-1);
+    }
+    cout << "Reading file..." << endl;
+
+    /* width of the bean */
+    file >> width;
+
+    /* number of pieces/polygons */
+    int numberOfPolygons;
+    file >> numberOfPolygons;
+    vector<polygon> polygons(numberOfPolygons);
+
+    /* input all the polygons */
+    for (auto &p : polygons)
+    {
+        file >> p.n;
+        p.v.resize(p.n);
+        for (auto &vi : p.v)
+        {
+            file >> vi.x >> vi.y;
+        }
+    }
+    cout << "Reading complete" << endl;
+    file.close();
+    return polygons;
 }
