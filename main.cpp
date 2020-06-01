33ce096646ada074be04c36063874cd01b756293
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2020-06-02 00:28:14 +0600

    Update main Cmake

--- main.cpp
+++ main.cpp
@@ -7,13 +7,13 @@ int main(int argc, char const *argv[])
 {
     if (argc == 2)
     {
-        // double width = 0;
-        // vector<polygon> polygons = io::readInputFile(string(argv[1]), width);
-        // cout << "Bean width: " << width << endl;
-        // for (auto p : polygons)
-        // {
-        //     p.print();
-        // }
+        double width = 0;
+        vector<Item> items = io::readItemsFromInputFile(string(argv[1]), width);
+        cout << "Bean width: " << width << endl;
+        for (auto p : items)
+        {
+            p.print();
+        }
     }
     else
     {
