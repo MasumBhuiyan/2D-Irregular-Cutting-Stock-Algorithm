10f8392e102006e5fdd2b0eb596191d1a63475f2
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2020-06-06 22:42:03 +0600

    namespace linear and namespace polygonal added

--- include/geometry.hpp
+++ include/geometry.hpp
@@ -2,28 +2,43 @@
 #define GEOMETRY_HPP
 
 #include <cmath>
+#include <vector>
 
 #include "Point.hpp"
 
 namespace geo
 {
-const double PI = acos(-1);
-const double EPS = 1e-8;
+    const double PI = acos(-1);
+    const double EPS = 1e-8;
 
-int dcmp(double, double eps = EPS); // double compare
-double DEG2RAD(double);             // degree to radian
-double RAD2DEG(double);             // radian to degree
+    int dcmp(double, double eps = EPS); // double compare
+    double DEG2RAD(double);             // degree to radian
+    double RAD2DEG(double);             // radian to degree
 
 }; // namespace geo
 
 typedef Point Vector;
 namespace vec
 {
-double dot(Vector a, Vector b);
-double cross(Vector a, Vector b);
-double lenSq(Vector a);
-double len(Vector a);
-double getAngleInRad(Vector a, Vector b);
+    double dot(Vector, Vector);           // dot product
+    double cross(Vector, Vector);         // cross product
+    double lenSq(Vector);                 // lenth square
+    double len(Vector);                   // length
+    double getAngleInRad(Vector, Vector); // angle inside two vector
+
 }; // namespace vec
+
+namespace linear
+{
+    bool isOnSegment(Point, Point, Point); // returns true if the 1st Point is on the segment of 2nd Point & 3rd Point
+
+}; // namespace linear
+
+typedef std::vector<Point> Polygon;
+namespace polygonal
+{
+    bool isPointInsidePolygon(Point, Polygon &); // returns true if the Point is inside the Polygon
+
+}; // namespace polygonal
 
 #endif // GEOMETRY_HPP
