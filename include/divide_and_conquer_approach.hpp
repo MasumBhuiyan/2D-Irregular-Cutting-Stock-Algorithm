a266e83d2d2d04c122cfddcc2dcf8ed1821b8efa
MasumBhuiyan <masumbhuiyan577@gmail.com>
2020-06-30 10:22:03 +0600

    implemented merge heuristics exceptouterface function implementation

--- include/divide_and_conquer_approach.hpp
+++ include/divide_and_conquer_approach.hpp
@@ -11,8 +11,12 @@ typedef std::pair<Item, double> ItemState;
 
 namespace approach1
 {
+	int orient(Point, Point, Point);
+	std::pair<Point,Point> findLargestEdge(Item &);
+	double findRotationAngle(Point, Point);
+	Item placement(Item, Point, Point, Point, Point);
+	Item outerface(Item, Item, Point, Point, Point, Point);
     Item mergeHeuristic1(Item &, Item &);
-
     std::pair<double, double> minAreaRectangle(Item &);
     ItemState mergeItem(ItemState &, ItemState &);
     ItemState split(std::vector<Item> &, int, int);
--- src/divide_and_conquer_approach.cpp
+++ src/divide_and_conquer_approach.cpp
@@ -1,5 +1,81 @@
 #include "divide_and_conquer_approach.hpp"
 
+
+/**
+ * returns 1 if left, -1 otherwise
+*/
+int approach1::orient(Point a, Point b, Point c) 
+{
+    int p = (b - a).determinant(c - a);
+    return p > 0 ? 1 : -1;
+}
+
+/**
+ *finds the largest edge of an item
+*/
+std::pair<Point,Point> approach1::findLargestEdge(Item &item)
+{
+    int noOfVertices = item.vertices.size();
+    double d = 0;
+    Point p, q;
+
+    for(int i = 0; i < noOfVertices; i += 1) 
+    {
+        Point _p = item.vertices[ i ];
+        Point _q = item.vertices[ (i + 1) % noOfVertices ];
+        double _d = (_p.x - _q.x) * (_p.x - _q.x) + (_p.y - _q.y) * (_p.y - _q.y);
+        if( _d > d )
+        {
+            d = _d;
+            p = _p;
+            q = _q;
+        }
+    }
+    return {p, q};
+}
+
+/** 
+ * finds the angle in radian between pq and rs
+*/
+double approach1::findRotationAngle(Point pq, Point rs)
+{
+    double lengthOfPQ = sqrt((pq.x * pq.x) + (pq.y * pq.y)); 
+    double lengthOfRS = sqrt((rs.x * rs.x) + (rs.y * rs.y)); 
+    double angle = acos(((pq.x * rs.x) + (pq.y * rs.y)) / (lengthOfPQ * lengthOfRS));
+    return angle;
+}
+
+/**
+ * let say p, q are the endpoints of Item A's largest edge
+ * let say r, s are the endpoints of Item B's largest edge
+ * we want to place Item B's point r at point p and return the resultant Item b
+*/
+Item approach1::placement(Item item, Point p, Point q, Point r, Point s)
+{
+    double angle = findRotationAngle(q - p, s - r);
+    int isLeft = orient(r, s, q); 
+    for(auto &point : item.vertices)
+    {
+        point = point.rotate(r, angle * isLeft); 
+        point = point - r;
+        point = point + p;
+    }
+    return item;
+}
+
+/**
+ * returns the outerface of the Item a, and b after placing 
+ * Item b's point r at Item a's point p
+*/
+Item approach1::outerface(Item a, Item b, Point p, Point q, Point r, Point s)
+{
+    Item outerface;
+    return outerface;
+}
+
+/**
+ * returns the min bounding rectangle axis parallel
+*/
 std::pair<double, double> approach1::minAreaRectangle(Item &item)
 {
     double min_x = INF, max_x = -INF;
@@ -17,13 +93,46 @@ std::pair<double, double> approach1::minAreaRectangle(Item &item)
     return {l, w};
 }
 
+/**
+ * merges two item by proposed heuristic1
+*/
 Item approach1::mergeHeuristic1(Item &item1, Item &item2)
 {
     Item mergedItem;
 
+    auto pq = findLargestEdge(item1);
+    auto rs = findLargestEdge(item2);
+    p = pq.first, q = pq.second;
+    r = rs.first, s = rs.second;
+    
+    Item item2PR = placement(item2, p, q, r, s);
+    Item item2PS = placement(item2, p, q, s, r);
+    Item item2QR = placement(item2, q, p, r, s);
+    Item item2QS = placement(item2, q, p, s, r);
+
+    vector<Item> outerfaces({ 
+        outerface(item1, item2PR, p, q, r, s), 
+        outerface(item1, item2PS, p, q, s, r),
+        outerface(item1, item2QR, q, p, r, s), 
+        outerface(item1, item2QS, q, p, s, r)});
+    
+    std::vector<tuple<double,double,int>> areas;
+    for(int i = 0; i < 4; i += 1) 
+    {
+        auto minRectangle = minAreaRectangle(outerfaces[ i ]);
+        areas.push_back({minRectangle.first * minRectangle.second, minRectangle.first, i}); 
+    }
+    sort(areas.begin(), areas.end());
+
+    int length, width, i;
+    tie(length, width, i) = areas[ 0 ];
+    mergedItem = outerfaces[ i ];
     return mergedItem;
 }
 
+/**
+ * merge items by trying different rotations
+*/
 ItemState approach1::mergeItem(ItemState &itemState1, ItemState &itemState2)
 {
     double angle = 90;
@@ -64,6 +173,9 @@ ItemState approach1::mergeItem(ItemState &itemState1, ItemState &itemState2)
     return {mergedItem, minArea};
 }
 
+/**
+ * divide and conquer approach
+*/
 ItemState approach1::split(std::vector<Item> &items, int left, int right)
 {
     if (left == right)
@@ -78,6 +190,9 @@ ItemState approach1::split(std::vector<Item> &items, int left, int right)
     return mergeItem(resultLeft, resultRight);
 }
 
+/**
+ * returns soltuion for different items order
+*/
 void approach1::solution(std::vector<Item> &items)
 {
     ItemState result = split(items, 0, items.size() - 1);
