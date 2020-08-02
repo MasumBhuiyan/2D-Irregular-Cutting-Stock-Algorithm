db35a2e22d645c887fd1474c4fb6d3d64bf708e1
masumBhuiyan <masumbhuiyan577@gmail.com>
2020-08-02 23:35:44 +0600

    wrote some headers not completed for the whole algorithm, partial prototype completed

--- include/bin_packing.hpp
+++ include/bin_packing.hpp
@@ -36,9 +36,45 @@ const double PI = acos(-1);
 
 static int frameno;
 
-namespace bin_packing
+
+std::vector<vector<Point>> allPairDominantPoints;
+std::vector<vector<Polygon>> allPairNoFitPolygons;
+std::vector<vector<double>> allPairClusterValues;
+std::vector<Polygon> allInnerFitPolygons;
+std::vector<vector<Polygon>> convexHullVacancies;
+std::vector<bool> convavity;
+
+namespace guided_cuckoo_search
 {
-    std::tuple<std::vector<Polygon>, double> readDataset(std::string);
+    std::tuple<std::vector<Polygon>, double> readDataset(std::string); // 1
+    std::tuple<std::vector<Polygon>> gcs(std::vector<Polygon>&, double); // 2
+    std::tuple<std::vector<Point>, std::vector<double>> generateInitialSolution(std::vector<Polygon>&, double); // 3
+    std::tuple<std::vector<Point>, std::vector<double>> cluster(std::vector<Polygon>&); // 4
+    void sort(std::vector<vector<Polygon>>&); // 5
+    std::vector<Polygon> blf(std::vector<vector<Polygon>>&); // 6
+    Polygon translatePolygon(Polygon&, Point); // 7
+    std::vector<Polygon> translatePolygons(std::vector<Polygon>&, Point); // 8
+    Polygon rotatePolygon(Polygon&, double); // 9
+    std::vector<Polygon> rotatePolygons(std::vector<Polygon>&, double); // 10
+    void calculateAllPairNoFitPolygons(std::vector<Polygon>&); // 11
+    void calculateAllInnerFitPolygons(std::vector<Polygon>&, double, double); // 12
+    Polygon calculateNoFitPolygon(Polygon&, Polygon&); // 13
+    Polygon calculateInnerFitPolygon(Polygon&, double, double); // 14
+    bool isConcave(Polygon&); // 15
+    void calculateConcavityOfAllNoFitPolygons(std::vector<Polygon>&); // 16
+    Point findDominantPoint(Polygon&); // 17
+    std::vector<Polygon> findConvexHullVacancy(Polygon&); // 18
+    double calulateClusteringCriteria1(Polygon&, Polygon&); // 19
+    double calulateClusteringCriteria2(Polygon&, Polygon&); // 20
+    double calulateClusterValue(Polygon&, Polygon&); // 21
+    Polygon convexHull(std::vector<Point>&); // 22
+    tuple<std::vector<Point>, std::vector<double>> perfectMatching(double); // 23 // cluster percentage
+    Point findBlfPoint(std::vector<Polygon>&, std::vector<Polygon>&); // 24 // current all ready placed polygons, polygon or polygons next to be placed. 
+                                                                      //find all the places this polygon/polygons can be place. basically all NFP-IFP intersection points and vertices
+                                                                      // return the bottom left Point among the points
+    bool isItPossibleToPlacePolygonAt(std::vector<Polygon>&, Polygon, Point); // 25 // current placed polygons, polygon next to be placed at Point
+    double polygonPolygonIntersectionArea(Polygon&, Polygon&); // 26
+    double linePointDistance(Point, Point, Point);// 27
 };
 
 #endif
