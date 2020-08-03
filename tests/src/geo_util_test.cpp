d72ecd1cb154598825876bad1922ffa613819e35
masumBhuiyan <masumbhuiyan577@gmail.com>
2020-08-03 17:19:15 +0600

    created geo_util_test.cpp

---
+++ tests/src/geo_util_test.cpp
@@ -0,0 +1,88 @@
+#include <geo_util.hpp>
+#include <custom_assertion.hpp>
+
+void dblcmp_test()
+{
+    
+}
+void isConcave_test()
+{
+
+}
+void getLength_test()
+{
+
+}
+void getWidth_test()
+{
+
+}
+void crossProduct_test()
+{
+
+}
+void normalize_test()
+{
+
+}
+void orientation_test()
+{
+
+}
+void rotatePolygon_test()
+{
+
+}
+void translatePolygon_test()
+{
+
+}
+void linePointDistance_test()
+{
+
+}
+void polygonPolygonIntersectionArea_test()
+{
+
+}
+void segmentSegmentIntersectionPoint_test()
+{
+
+}
+void rotatePolygons_test()
+{
+
+}
+void translatePolygons_test()
+{
+
+}
+void isItPossibleToPlacePolygon_test()
+{
+
+}
+void pointInRectangle_test()
+{
+
+}
+
+int main()
+{
+    dblcmp_test();
+    isConcave_test();
+    getLength_test();
+    getWidth_test();
+    crossProduct_test();
+    normalize_test();
+    orientation_test();
+    rotatePolygon_test();
+    translatePolygon_test();
+    linePointDistance_test();
+    polygonPolygonIntersectionArea_test();
+    segmentSegmentIntersectionPoint_test();
+    rotatePolygons_test();
+    translatePolygons_test();
+    isItPossibleToPlacePolygon_test();
+    pointInRectangle_test();
+    return 0;
+}
