11bf18d3bff579a3e8cc807c71ef7abecd081503
masumBhuiyan <masumbhuiyan577@gmail.com>
2020-08-04 18:40:47 +0600

    deleted unnecessary prototypes

--- tests/src/cluster_util_test.cpp
+++ tests/src/cluster_util_test.cpp
@@ -24,26 +24,6 @@ Polygon readTestcase(std::string testcaseName)
     return polygon;
 }
 
-Polygon convexHull(MultiPolygon); 
-    std::vector<Polygon> findConvexHullVacancy(Polygon&);
-    std::vector<std::vector<Point>> findOppositeSideOfVacancies(Polygon &, std::vector<Polygon> &);
-    Point findDominantPoint(Polygon&);
-    std::vector<std::vector<Point>> findAllPairDominantPoint(std::vector<std::vector<Polygon>> &);
-    std::vector<std::vector<Polygon>> findAllConvexHullVacancies(std::vector<Polygon>&);
-    double clusteringCriteria1(Polygon&, Polygon&);
-    double clusteringCriteria2(Polygon&, Polygon&);
-    double getClusterValue(Polygon&, Polygon&);
-    void sort(std::vector<std::vector<Polygon>>&);
-    std::vector<Point> getCandidatePlacementPositions(std::vector<Polygon> &, std::vector<Polygon>);
-    Point findBlfPoint(std::vector<Polygon>&, std::vector<Polygon>&);
-    std::vector<Polygon> blf(std::vector<std::vector<Polygon>>&);
-    double getBestClusters(std::vector<std::vector<double>> &, std::vector<double>&, int, int);
-    void printBestClusters(std::vector<std::vector<double>> &, std::vector<double>&, int, int, std::vector<std::tuple<int,int>>&);
-    std::vector<std::tuple<int,int>> perfectClustering(std::vector<std::vector<double>> &, double);
-    std::vector<std::vector<double>> getClusterValues(std::vector<Polygon>&);
-    std::vector<Polygon> generateInitialSolution(std::vector<Polygon>&, double);
-
-
 void convexHull_test() 
 {
 
