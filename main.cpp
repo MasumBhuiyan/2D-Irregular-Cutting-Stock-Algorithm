ac15e56bf829bbf5bece74f692a781aa96e05541
masumBhuiyan <masumbhuiyan577@gmail.com>
2020-08-17 01:06:59 +0600

    added initialSolutions folder

--- main.cpp
+++ main.cpp
@@ -3,15 +3,15 @@
 int main(int argc, char const *argv[])
 {
     bin_packing::runDataset("../tests/dataset/", "sample", 20);
-    bin_packing::runDataset("../tests/dataset/", "dighe1", 200);
-    bin_packing::runDataset("../tests/dataset/", "dagli", 120);
-    bin_packing::runDataset("../tests/dataset/", "poly1a", 100);
-    bin_packing::runDataset("../tests/dataset/", "blaz", 29);
-    bin_packing::runDataset("../tests/dataset/", "jakobs1", 12);
-    bin_packing::runDataset("../tests/dataset/", "jakobs2", 8);
-    bin_packing::runDataset("../tests/dataset/", "marques", 84);
-    bin_packing::runDataset("../tests/dataset/", "shirts", 62);
-    bin_packing::runDataset("../tests/dataset/", "trousers", 300);
-    bin_packing::runDataset("../tests/dataset/", "albano", 11000);
+    // bin_packing::runDataset("../tests/dataset/", "dighe1", 200);
+    // bin_packing::runDataset("../tests/dataset/", "dagli", 120);
+    // bin_packing::runDataset("../tests/dataset/", "poly1a", 100);
+    // bin_packing::runDataset("../tests/dataset/", "blaz", 29);
+    // bin_packing::runDataset("../tests/dataset/", "jakobs1", 12);
+    // bin_packing::runDataset("../tests/dataset/", "jakobs2", 8);
+    // bin_packing::runDataset("../tests/dataset/", "marques", 84);
+    // bin_packing::runDataset("../tests/dataset/", "shirts", 62);
+    // bin_packing::runDataset("../tests/dataset/", "trousers", 300);
+    // bin_packing::runDataset("../tests/dataset/", "albano", 11000);
     return 0;
 }
