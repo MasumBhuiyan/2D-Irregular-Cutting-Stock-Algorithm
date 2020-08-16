d8dd52ae9eb47362cffa96600980f00a919deca2
masumBhuiyan <masumbhuiyan577@gmail.com>
2020-08-16 18:25:12 +0600

    updated && deleted

--- main.cpp
+++ main.cpp
@@ -21,8 +21,8 @@ int main(int argc, char const *argv[])
     mkdir(outputLocation.c_str(), 0777);
 
     // testDataset("sample1", argc == 0);
-    // testDataset("sample", argc == 0);  // 100.00% // no cluster: 50.00% //
-    // testDataset("albano", argc == 0);  //  67.89% // no cluster: 80.26% //
+    // testDataset("sample", argc == 0);  // 100.00% // no cluster: 50.00% // previous: 66.67%
+    // testDataset("albano", argc == 0);  //  67.89% // no cluster: 80.26% // 
     // testDataset("blaz", argc == 0);    //  74.06% // no cluster: 69.67% //
     // testDataset("dagli", argc == 0);   //  70.13% // no cluster: 73.67% //
     // testDataset("dighe1", argc == 0); //  67.42% // no cluster: 66.71% //
@@ -32,3 +32,25 @@ int main(int argc, char const *argv[])
     // testDataset("poly1a", argc == 0);  //  55.92% // no cluster: 57.86% //
     return 0;
 }
+
+/*
+
+
+experiment result
+             [Previous Result]    [generateInitialSolution()]
+                                    [cluster] | [no cluster]
+===============================================================================
+sample....:     [ 66.67%]           [ 50.00%] | [ 50.00%]
+albano....:     [ 79.62%]           [ 67.89%] | [ 80.26%]
+blaz......:     [ 74.92%]           [ 74.06%] | [ 69.67%]
+dagli.....:     [ 66.78%]           [ 70.13%] | [ 73.67%]
+dighe1....:     [ 60.48%]           [ 67.42%] | [ 66.71%]
+jakobs1...:     [ 80.50%]           [ 68.92%] | [ 72.37%]
+jakobs2...:     [ 69.02%]           [ 57.69%] | [ 69.01%]
+trousers..:     [ 78.07%]           [ 63.87%] | [ 82.50%]
+poly1a....:     [ 36.14%]           [ 55.92%] | [ 57.86%]
+shirts....:     [ 87.10%]           [ ------] | [ ------]
+marques...:     [ 82.12%]           [ ------] | [ ------]
+
+
+*/
